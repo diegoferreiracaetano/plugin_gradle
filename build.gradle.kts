534b133f6371d028086d7916c3c8e0347560f150
diego ferreira caetano <di.cafe@hotmail.com>
2020-02-19 00:45:35 -0300

    change to object, update gradle and list group

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,21 +4,36 @@ plugins {
     java
     maven
     kotlin("jvm") version "1.3.60"
+    id("jacoco")
 }
 
 group = "com.diegoferreiracaetano"
 version = "0.0.11"
 
+
 apply {
     from("$rootDir/tools/ktlint.gradle")
     from("$rootDir/tools/base.gradle")
 }
-
 repositories {
     mavenCentral()
     jcenter()
 }
 
+buildscript {
+
+    repositories {
+        google()
+        jcenter()
+        mavenCentral()
+        maven("https://oss.sonatype.org/content/repositories/snapshots")
+    }
+
+    dependencies{
+        classpath("org.sonarsource.scanner.gradle:sonarqube-gradle-plugin:2.7")
+        classpath("org.jacoco:org.jacoco.core:0.8.5")
+    }
+}
 
 dependencies {
     implementation(gradleApi())
