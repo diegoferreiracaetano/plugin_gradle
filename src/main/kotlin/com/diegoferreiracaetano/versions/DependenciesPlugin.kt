d12d2fab8fb63ab8020d5d767e709a81d1c48fab
diego ferreira caetano <di.cafe@hotmail.com>
2020-02-24 21:49:29 -0300

    add jacoco dsl

--- src/main/kotlin/com/diegoferreiracaetano/versions/DependenciesPlugin.kt
+++ src/main/kotlin/com/diegoferreiracaetano/versions/DependenciesPlugin.kt
@@ -7,7 +7,11 @@ import com.diegoferreiracaetano.versions.dependencies.LibsExtension
 import com.diegoferreiracaetano.versions.dependencies.TestExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.testing.Test
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.withType
+import org.gradle.testing.jacoco.plugins.JacocoTaskExtension
+import org.gradle.testing.jacoco.tasks.JacocoReport
 import java.io.File
 import java.io.FileInputStream
 import java.util.Properties
@@ -94,6 +98,81 @@ class DependenciesPlugin : Plugin<Project> {
                         isReturnDefaultValues = true
                         isIncludeAndroidResources = true
                     }
+                }
+
+                jacoco.version = Versions.JACOCO
+            }
+
+        }
+
+        project.configure<JacocoTaskExtension> {
+
+            project.tasks.withType<Test> {
+                this@configure.isIncludeNoLocationClasses = true
+                this@configure.excludes = listOf("jdk.internal.*")
+            }
+
+            val fileFilter = listOf(
+                "**/R.class",
+                "**/R$*.class",
+                "**/BuildConfig.*",
+                "**/Manifest*.*",
+                "**/*Test*.*",
+                "**/*Entity*.*",
+                "android/**/*.*",
+                "**/di/**"
+            )
+
+            val kotlinTree = project.fileTree(
+                mapOf(
+                    "dir" to "${project.buildDir}/tmp/kotlin-classes/debug",
+                    "excludes" to fileFilter
+                )
+            )
+
+            val javaTree = project.fileTree(
+                mapOf(
+                    "dir" to "${project.buildDir}/intermediates/classes/debug",
+                    "excludes" to fileFilter
+                )
+            )
+
+            project.tasks.withType<JacocoReport> {
+                group = "Reporting"
+                description =
+                    "Run tests and generate coverage report for instrumented and jvm testes"
+                dependsOn("testDebugUnitTest")
+
+                reports {
+                    it.xml.isEnabled = true
+                    it.xml.destination =
+                        project.file("${project.rootProject.buildDir}/reports/${project.name}/jacocoTestReport.xml")
+                    it.html.isEnabled = true
+                }
+
+                classDirectories.from(project.files(listOf(javaTree, kotlinTree)))
+
+                val coverageSourceDirs = listOf("src/main/kotlin")
+
+                additionalSourceDirs.from(project.files(coverageSourceDirs))
+                sourceDirectories.from(project.files(coverageSourceDirs))
+
+                executionData.from(
+                    project.fileTree(
+                        mapOf(
+                            "dir" to "$project.buildDir",
+                            "includes" to listOf(
+                                "jacoco/*.exec",
+                                "outputs/code_coverage/**/connected/*.ec",
+                                "tmp/tests/*.exec",
+                                "tmp/tests/*.ec"
+                            )
+                        )
+                    )
+                )
+
+                doLast {
+                    println("Jacoco report has been generated to file://${reports.html.destination}")
                 }
             }
         }
