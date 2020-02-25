0463ebed9081594daee9b66fe73e42206be0e3df
diego ferreira caetano <di.cafe@hotmail.com>
2020-02-25 18:26:30 -0300

    commit main

--- .github/workflows/tags.sh
+++ .github/workflows/tags.sh
@@ -1,9 +1,5 @@
 get_latest_release() {
-  curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
-    grep '"tag_name":' |                                            # Get tag line
-    sed -E 's/.*"([^"]+)".*/\1/'                                    # Pluck JSON value
+  curl --silent "https://api.github.com/repos/$1/releases/latest" |
+    grep '"tag_name":' |
+    sed -E 's/.*"([^"]+)".*/\1/'
 }
-
-# Usage
-# $ get_latest_release "creationix/nvm"
-# v0.31.4
