3ed51d07ad90f956a96d7d2e7c30e84a150be660
diego ferreira caetano <di.cafe@hotmail.com>
2020-02-29 18:41:49 -0300

    - add versions modules

--- src/main/kotlin/com/diegoferreiracaetano/versions/ModuleConfig.kt
+++ src/main/kotlin/com/diegoferreiracaetano/versions/ModuleConfig.kt
@@ -4,14 +4,12 @@ object ModuleConfig {
 
     val app = Modules(
         "app",
-        "com.diegoferreiracaetano.driver.app",
         1,
         "0.0.1"
     )
 
     val style = Modules(
         "style",
-        "com.diegoferreiracaetano.driver.app",
         1,
         "0.0.1"
     )
